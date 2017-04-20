6e1fdbf71057d884e5dee3eb5af9ac1b436cfd29
山本省吾 <yama-sho@kc-no-MacBook-Pro-2.local>
2017-04-20 15:58:36 +0900

    rest apiを実装

---
+++ rest_api.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+tasks = [
+	    {
+	        'id': 1,
+	        'title': '日用品を買ってくる',
+	        'description': 'ミルク、チーズ、ピザ、フルーツ',
+	        'done': False
+	    },
+	    {
+	        'id': 2,
+	        'title': 'Python の勉強',
+	        'description': 'Python で Restful API を作る',
+	        'done': False
+	    }
+	]
+
+dreams = [
+	    {
+	        'id': 1,
+	        'title': 'プラモデルを買う',
+	        'description': 'FA、RG、PG、HG',
+	        'done': False
+	    },
+	    {
+	        'id': 2,
+	        'title': 'Python の勉強',
+	        'description': 'Python で Restful API を作る',
+	        'done': False
+	    }
+	]
+                                                                                                                                                                                                                                                                                                                                                                             
+@app.route('/')
+def index():
+	return 'Hello World!日本語'
+	
+
+	
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+	return jsonify({'tasks': tasks})
+	
+@app.route('/todo/api/v1.0/dreams', methods=['POST'])
+def get_dreams():
+	return jsonify({'dreams': dreams})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
